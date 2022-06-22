7e6ec8d0ae04500d7981a54f403e91ecd5626ed7
Michael Zapata <mikeyfriends@hotmail.com>
2022-06-22 22:58:13 +0100

    fixed some issues with code

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,5 +1,5 @@
 // see SignupForm.js for comments
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useMutation } from "@apollo/client";
 import { Form, Button, Alert } from "react-bootstrap";
 import { LOGIN_USER } from "../utils/mutations";
