986841cdc27c468df2bf1c833d914b0d0ddd4439
Michael Zapata <mikeyfriends@hotmail.com>
2022-06-22 22:48:10 +0100

    added queries and mutations

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  {
+    query GET_ME() {    
+        me {
+            _id
+            email
+            username
+            savedBooks {
+                bookId
+                description
+                title
+                link
+                authors
+                image
+            }
+        }
+    }
+  }
+`;
